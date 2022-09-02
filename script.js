36e4dc5442e3cd01013efae4babcf8e174a53574
Michael <choquemichael@gmail.com>
2022-09-02 00:42:34 -0300

    Create Js

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnmobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+const nav = document.getElementById('nav');
+nav.classList.toggle('active');
+}
+
+btnmobile.addEventListener('click', toggleMenu);
