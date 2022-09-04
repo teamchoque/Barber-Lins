a69712aba03d0e00c3ac0562c041c37c12f2a605
Michael <choquemichael@gmail.com>
2022-09-04 01:11:00 -0300

    create menu

--- script.js
+++
@@ -1,8 +0,0 @@
-const btnmobile = document.getElementById('btn-mobile');
-
-function toggleMenu() {
-const nav = document.getElementById('nav');
-nav.classList.toggle('active');
-}
-
-btnmobile.addEventListener('click', toggleMenu);
